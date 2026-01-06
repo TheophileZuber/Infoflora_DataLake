dd0d62ed69dd3ab41ac5ad2624324b523ccfc466
TheophileZuber <zuber.theo@gmail.com>
2026-01-06 11:45:21 +0100

    Good number of parameters per lake now

--- code/Metadata_Exploration_DataLake.R
+++ code/Metadata_Exploration_DataLake.R
@@ -34,7 +34,7 @@ for (i in Endpoints) {
   assign(var_name, df, envir = .GlobalEnv)
   
   #remove unecessary variables
-  rm(df, var_name, req_metadata, response)
+  rm(df, var_name, req_metadata, response,i)
 }
 
 ### Access to metadata on the parameters for each dataset and creat summary table
@@ -50,18 +50,27 @@ for (i in Endpoints) {
 file_ids <- unique(files_metadata$datasets_id)
 dataset_ids <- unique(datasets_metadata$id)
 
-cat("Find IDs in files_metadata but NOT in datasets_metadata") 
+### Find IDs in files_metadata but NOT in datasets_metadata 
 ids_only_in_files <- setdiff(file_ids, dataset_ids)
 ids_only_in_files
 # [1]   21   22  822   19   20   24   25   NA  807 1376 1375 1236 1237 1352 1353  929 1354  930  967  920 1268  922  923
 # [24] 1276  924  925  926  927  928 1240 1430 1378 1362 1429  645    9    5    8    7    6    3    4    2   10  675   15
 # [47]   11   14 1261  601
 
-cat("Find IDs in datasets_metadata but NOT in files_metadata")
+#subset to look at these specific IDs'rows
+SUB_ids_only_in_files <- files_metadata %>% filter(datasets_id == ids_only_in_files)
+setdiff(unique(SUB_ids_only_in_files$datasets_id),ids_only_in_files)
+#' datasets without metadata, lots of JSON files URL to download from web not through API
+
+### Find IDs in datasets_metadata but NOT in files_metadata
 ids_only_in_datasets <- setdiff(dataset_ids, file_ids)
 ids_only_in_datasets
 # [1] 1
 
+#subset to look at this specific ID's row
+SUB_ids_only_in_datasets <- datasets_metadata %>% filter(id == 1) #' Information on morphology of several lakes
+ 
+# Take only the IDs present in both files and datasets
 datasets_id <- unique(files_metadata$datasets_id)
 datasets_id <- datasets_id[!(datasets_id %in% ids_only_in_files)]
 
@@ -76,6 +85,19 @@ parameters <- selectiontables_metadata %>%
 filter(if_any(c(3:6), ~!is.na(.)))
 
 ### Create table with lake names and parameters metadata
+
+# Look at how many datasets per lakes
+dataset_lake_map <- datasets_metadata %>%
+  filter(id %in% datasets_id) %>%
+  left_join(lakes %>% select(id, name), by = c("lakes_id" = "id")) %>%
+  select(dataset_id = id, lakes_id, lake_name = name) %>%
+  filter(!is.na(lake_name))
+
+# Count datasets per lake
+Ndata_per_lake <- dataset_lake_map %>% 
+  count(lake_name, sort = TRUE)
+# ONLY 44 LAKE WITH DATA?
+
 
 # Create empty list to store results
 data_list <- list()
@@ -107,9 +129,15 @@ for (n in datasets_id) {
   response <- bind_rows(response)
   
   #store the dataframe to the right lake
-  data_list[[lake_name]] <- response
+  
+  if (lake_name %in% names(data_list)) {
+    data_list[[lake_name]] <- bind_rows(data_list[[lake_name]], response)
+  } else {
+    data_list[[lake_name]] <- response
+  }
 }
-
+#rm unecessary variables
+rm(rows,response,req_metadata,endpoints_URL,lake_name,lake_id)
 
 # Combine all parameters by lake
 lake_parameters_summary <- map_df(names(data_list), function(lake_name) {
@@ -119,7 +147,14 @@ lake_parameters_summary <- map_df(names(data_list), function(lake_name) {
 })
 
 
+
 ### presence/absence table
+
+# #check na
+# lake_parameters_summary[is.na(lake_parameters_summary$parseparameter),] no na
+# lake_parameters_summary[is.na(lake_parameters_summary$parameters_id),] no na
+
+#presence absence with combination of parameters'shortname and there parameter's id
 presence_absence <- lake_parameters_summary %>%
   distinct(lake, parseparameter, parameters_id) %>%  # unique combinations
   mutate(present = 1) %>%
@@ -130,6 +165,8 @@ presence_absence <- lake_parameters_summary %>%
     values_fill = 0
   )
 
+# presence absence with only parameter's id and with parameter's id replaced by their name
+
 parameterID_presence_absence <- lake_parameters_summary %>%
   distinct(lake, parameters_id) %>%  # unique combinations
   mutate(present = 1) %>%
@@ -139,3 +176,16 @@ parameterID_presence_absence <- lake_parameters_summary %>%
     values_from = present,
     values_fill = 0
   )
+
+# Create a named vector for mapping
+name_mapping <- setNames(selectiontables_metadata$name, selectiontables_metadata$id)
+# Only rename numeric columns
+parameter_presence_absence <- parameterID_presence_absence %>%
+  rename_with(
+    ~name_mapping[.x],
+    .cols = matches("^\\d+$")  # Only columns that are purely numeric
+  )
+
+setwd("~/MasterBec/InfoFlora_stage/Infoflora_DataLake")
+save(lake_parameters_summary,presence_absence,parameterID_presence_absence, file = "data/presence_absence.RData")
+load("data/presence_absence.RData")
